e811c2f2ffb3ebfb1fbdecbe4f99764acc8a1a34
Radhika Rajput <rajputradhika485@gmail.com>
2026-07-23 19:30:43 +0530

    Create Solution.java

---
+++ 11-container-with-most-water/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        
+        while(left < right) {
+            int width = right - left;
+        
+
+            int currHeight = Math.min(height[left],height[right]);
+
+            int currArea = width * currHeight;
+
+            maxArea = Math.max(currArea,maxArea);
+
+            if(height[left]<height[right]) {
+                left++;
+
+            } else {
+                right--;
+            }
+    }
+
+            return maxArea;
+
+        
+        
+    }
+}
